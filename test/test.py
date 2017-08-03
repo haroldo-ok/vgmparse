adecf3ef505ce6bfaafdf21f7d255018579feb7b
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2017-08-02 21:19:37 -0300

    Created test for the saving function

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 import unittest
 import vgmparse
+import tempfile, time
 
 class TestParsing(unittest.TestCase):
     
@@ -15,6 +16,14 @@ class TestParsing(unittest.TestCase):
             # The second command is a SN76496 Latch/Data command
             self.assertEqual(0x50, ord(parser.command_list[1]['command']))
             self.assertEqual(0x80, ord(parser.command_list[1]['data']))
+            
+    def test_save(self):
+        with open('Alex Kidd in Miracle World - 01 - Title Screen.vgm', 'rb') as f:
+            file_data = f.read()
+            parser = vgmparse.Parser(file_data)
+            
+            with open('{0}/test-save-{1}.vgm'.format(tempfile.gettempdir(), time.time()), 'wb') as o:
+                parser.save(o)
         
 if __name__ == '__main__':
     unittest.main()
